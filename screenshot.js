8013a9b3f787a592f93b8c6d3e1f11628f0cda5c
allen40240 <53719283+allen40240@users.noreply.github.com>
2025-12-26 17:43:49 +0800

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,9 +1,18 @@
 const puppeteer = require('puppeteer');
 (async () => {
-  const browser = await puppeteer.launch();
+  // 增加 args: ['--no-sandbox'] 繞過環境限制
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
+  
+  // 設定視窗大小
   await page.setViewport({ width: 500, height: 800 });
+  
+  // 前往目標網頁
   await page.goto('https://www.cingkang.com/schedule_auo.php', { waitUntil: 'networkidle2' });
-  await page.screenshot({ path: 'schedule.png' }); // 存成固定檔名
+  
+  // 截圖並儲存
+  await page.screenshot({ path: 'schedule.png' });
+  
   await browser.close();
+  console.log('截圖成功！');
 })();
